68e0b3ab0a8690a2469b878200fea0133fbcab2f
MTD-SONNT290\Dell <nguyenthaison0201@gmail.com>
2023-10-20 10:15:26 +0700

    add comment

--- libs/httphelper.h
+++ libs/httphelper.h
@@ -15,6 +15,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+// Print all the request message
 char *print_request(char *message, int len); // Variable declaration
 
 // Return an INT value referring to request type of a http request
@@ -25,6 +27,10 @@ char *print_request(char *message, int len); // Variable declaration
     HTTP_DELETE 3
 */
 int request_type(char *request);
+
+// create response to client including html file
 char *create_response(char* link);
+
+// get parameter value of 'params' in the request  
 char* param_val_GET(char* request, char* params);
 #endif
